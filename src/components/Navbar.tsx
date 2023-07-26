12547d9070e66b2e02f11c108b1ddb48ed3f8244
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-07-26 22:09:02 +0530

    Mobile Navigation Created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import Menu from './Menu';
+import Link from 'next/link';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="text-red-500 p-4 h-12 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+      <div className="text-lg">
+        <Link href="/">Caffino</Link>
+      </div>
+      <div>
+        <Menu />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
