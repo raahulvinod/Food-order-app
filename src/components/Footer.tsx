cf9f86311909723248e730a13cd638216b359988
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-07-27 22:26:19 +0530

    footer Added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import Link from 'next/link';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:40 text-red-500 flex items-center justify-between">
+      <Link href="/" className="font-bold text-xl">
+        Caffino
+      </Link>
+      <p> ALL RIGHTS RESERVED</p>
+    </div>
+  );
 };
 
 export default Footer;
