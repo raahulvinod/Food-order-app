35d642cca8bfcc571b1d9b3bf317935e568168dc
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-07-30 16:30:01 +0530

    Login Page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -10,7 +10,9 @@ const CartPage = () => {
         <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p1.png" alt="" width={100} height={100} />
           <div className="">
-            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <h1 className="uppercase text-xl font-bold text-gray-600">
+              sicilian
+            </h1>
             <span>Large</span>
           </div>
           <h2 className="font-bold">$79.90</h2>
@@ -20,7 +22,9 @@ const CartPage = () => {
         <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p3.png" alt="" width={100} height={100} />
           <div className="">
-            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <h1 className="uppercase text-xl font-bold text-gray-600">
+              sicilian
+            </h1>
             <span>Large</span>
           </div>
           <h2 className="font-bold">$79.90</h2>
@@ -30,7 +34,9 @@ const CartPage = () => {
         <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p2.png" alt="" width={100} height={100} />
           <div className="">
-            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <h1 className="uppercase text-xl font-bold text-gray-600">
+              sicilian
+            </h1>
             <span>Large</span>
           </div>
           <h2 className="font-bold">$79.90</h2>
@@ -38,7 +44,7 @@ const CartPage = () => {
         </div>
       </div>
       {/* PAYMENT CONTAINER */}
-      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6 text-gray-950">
         <div className="flex justify-between">
           <span className="">Subtotal (3 items)</span>
           <span className="">$81.70</span>
