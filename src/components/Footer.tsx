5f63a28fe0e400a0fa734bded06429268fee5d34
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-07-30 14:02:02 +0530

    Single Product Page Added and Footer Updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,55 @@
-import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:p-20 xl:40 text-red-500 flex items-center justify-between">
-      <Link href="/" className="font-bold text-xl">
-        Caffino
-      </Link>
-      <p> &copy; ALL RIGHTS RESERVED</p>
-    </div>
+    <>
+      <div className=" bg-red-500">
+        <div className="max-w-2xl mx-auto text-white py-10">
+          <div className="text-center">
+            <h3 className="text-3xl mb-3"> Download our Caffino app </h3>
+            <p> Stay order. All day, every day. </p>
+            <div className="flex justify-center my-10">
+              <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2">
+                <Image
+                  alt="play store"
+                  src="https://cdn-icons-png.flaticon.com/512/888/888857.png"
+                  className="w-7 md:w-8"
+                  width={20}
+                  height={20}
+                />
+                <div className="text-left ml-3">
+                  <p className="text-xs text-gray-200">Download on </p>
+                  <p className="text-sm md:text-base"> Google Play Store </p>
+                </div>
+              </div>
+              <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-44 mx-2">
+                <Image
+                  alt="apple store"
+                  src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
+                  className="w-7 md:w-8"
+                  width={20}
+                  height={20}
+                />
+                <div className="text-left ml-3">
+                  <p className="text-xs text-gray-200">Download on </p>
+                  <p className="text-sm md:text-base"> Apple Store </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-white">
+            <p className="order-2 md:order-1 mt-8 md:mt-0">
+              &copy; All rights reserved, 2023.
+            </p>
+            <div className="order-1 md:order-2 ">
+              <span className="px-2">About us</span>
+              <span className="px-2 border-l">Contact us</span>
+              <span className="px-2 border-l">Privacy Policy</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
